09286385c8175d9ca98cd4aff93fa5a41be612bb
“Rhiddhi <rhiddhi.simkhada@gmail.com>
2025-12-05 17:17:01 +0545

    Setup basic backend structure and server

--- model/user.model.js
+++ model/user.model.js
@@ -31,4 +31,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export const User = mongoose.model("User", userchema);
+export const User = mongoose.model("User", userSchema);
