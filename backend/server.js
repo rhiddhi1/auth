f9370b904b83b02d308bd4f2d22b2a3a811e7afb
“Rhiddhi <rhiddhi.simkhada@gmail.com>
2026-01-08 21:26:08 +0545

    add cookie-parser middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { connectDB } from "./db/connectDB.js";
 
 import authRoutes from "./routes/auth.route.js";
 
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
@@ -15,7 +17,8 @@ const PORT = process.env.PORT || 5000;
 //   console.log(connectDB.connection);
 // });
 
-app.use(express.json()); // allows us to parse inconing requests: req.body, we can grab the date from user eg: const { username, password, name } = req.body;
+app.use(express.json()); // allows us to parse incoming requests: req.body, we can grab the date from user eg: const { username, password, name } = req.body;
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
