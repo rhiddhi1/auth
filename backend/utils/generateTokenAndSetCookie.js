852737a00e6923ac443bffe478345192a9c5560f
“Rhiddhi <rhiddhi.simkhada@gmail.com>
2026-01-10 16:56:51 +0545

    Add generateTokenAndSetCookie

---
+++ backend/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (res, userID) => {
+  const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+    expiresIn: "7D",
+  });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
+};
